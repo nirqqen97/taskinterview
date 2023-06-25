b839f0d5c6fff45c4e85c4b5c0f8fd35d00856fc
nirqqen97 <nirqqen97@gmail.com>
2023-06-25 13:56:16 +0200

    Add Card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,40 @@
+import {CardView, CircleImage, DownSidePart,Image, Info, Line, UpSidePart, Button } from "./Card.styled"
+import messagePhoto from "../Images/pic.svg";
+import testPic from "../Images/Hansel.svg";
+
+
+export const Card = ( ) =>  {
+    // const handleClick = () => { 
+    // if (user.isFollowed) {
+    //     unSubscribe(id)
+    //     return
+    // }
+    //     setFollowers(id)
+       
+    // }
+    return ( 
+        <CardView>
+        
+        <UpSidePart>
+            <Image src={messagePhoto} alt="logo image" />
+        </UpSidePart>
+        <Line>
+            <CircleImage src= {testPic} width = "79" height= "79" />
+        </Line>
+        <DownSidePart>
+            <Info> user.tweets TWEETS</Info>
+            <Info> user.followers FOLLOWERS</Info>
+            <Info> user.followers FOLLOWERS</Info>
+            {/* <Button type="button"   followed={user.isFollowed} onClick={() => handleClick(user.id)}>
+               {user.isFollowed ? 'Following' : 'Follow'} 
+            </Button> */}
+            <Button type="button"   followed={false} >
+              Upvote 
+            </Button>
+            <Button type="button"   followed={false} >
+              DownVote 
+            </Button>
+        </DownSidePart>
+        </CardView>
+    )
+}
