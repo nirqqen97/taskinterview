e3644fa1e1b99c3e8f2cf22f6ea4d34d81e7f0d9
nirqqen97 <nirqqen97@gmail.com>
2023-06-29 18:48:46 +0200

    Add redirecting page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.styled.jsx";
 import { StreamerList } from "./Pages/StreamerList/StreamerList.jsx";
 import { StreamerDescription } from "./Pages/SreamerDescription/StreamerDescription.jsx";
+import { Home } from "./Pages/Home/Home.jsx";
 
 function App() {
   return (
     <BrowserRouter basename="taskinterview">
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/streamers" element={<StreamerList />} />
         <Route
           path="/streamers/:streamerId"
